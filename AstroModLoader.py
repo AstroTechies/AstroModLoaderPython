fe43ee0533ed582b0eef058eab9a451a6c9c8243
konsti219 <37149441+konsti219@users.noreply.github.com>
2020-08-04 20:56:55 +0200

    paths

---
+++ AstroModLoader.py
@@ -0,0 +1,37 @@
+import os
+
+from cogs.AstroModPak import PakParser
+import cogs.AstroAPI as AstroAPI
+
+
+class AstroModLoader():
+
+    def __init__(self):
+        print("Astro mod loader v0.1")
+
+        self.downloadPath = os.getcwd()
+        if not os.path.exists(os.path.join(self.downloadPath, "mods")):
+            os.makedirs(os.path.join(self.downloadPath, "mods"))
+        self.downloadPath = os.path.join(self.downloadPath, "mods")
+
+        self.modPath = os.path.join(
+            os.getenv('LOCALAPPDATA'), "Astro", "Saved")
+        if not os.path.exists(os.path.join(self.modPath, "Paks")):
+            os.makedirs(os.path.join(self.modPath, "Paks"))
+        self.modPath = os.path.join(self.modPath, "Paks")
+
+        print(self.modPath)
+
+
+if __name__ == "__main__":
+    try:
+        os.system("title AstroLauncher - Unofficial Dedicated Server Launcher")
+    except:
+        pass
+    try:
+        AstroModLoader()
+    except KeyboardInterrupt:
+        pass
+    except Exception as err:
+        print("ERROR")
+        print(err)
