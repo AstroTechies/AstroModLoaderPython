833ed614a21379cf51c9cb47d170e680c40b9214
konsti219 <37149441+konsti219@users.noreply.github.com>
2020-08-05 20:39:39 +0200

    active mods implementation

--- AstroModLoader.py
+++ AstroModLoader.py
@@ -25,6 +25,14 @@ class AstroModLoader():
             os.makedirs(os.path.join(self.installPath, "Paks"))
         self.installPath = os.path.join(self.installPath, "Paks")
 
+        if not os.path.exists(os.path.join(self.downloadPath, "modconfig.json")):
+            with open(os.path.join(self.downloadPath, "modconfig.json"), 'w') as f:
+                f.write('{"mods":[]}')
+
+        self.modConfig = {}
+        with open(os.path.join(self.downloadPath, "modconfig.json"), 'r') as f:
+            self.modConfig = json.loads(f.read())
+
         # gather mod list (only files)
         print("gathering mod data...")
         self.mods = numpy.unique(self.getPaksInPath(
@@ -108,32 +116,78 @@ class AstroModLoader():
             else:
                 mod["metadata"]["linked_actor_components"] = []
 
+            # read data from modconfig.json
+            config = list(
+                filter(lambda m: m["mod_id"] == mod["metadata"]["mod_id"], self.modConfig["mods"]))
+            if len(config):
+                config = config[0]
+                if "update" in config:
+                    mod["update"] = config["update"]
+                else:
+                    mod["update"] = True
+
+                if "always_active" in config:
+                    mod["always_active"] = config["always_active"]
+                else:
+                    mod["always_active"] = False
+            else:
+                mod["update"] = True
+                mod["always_active"] = False
+
             return mod
         print("parsing metadata...")
         self.mods = list(map(readModData, self.mods))
 
-        print(self.mods)
+        # print(self.mods)
 
         # TODO download updates
 
         while True:
-            # TODO clear install path
-
-            # TODO load all previously active mods back into mod path (with changes)
+            # clear install path
+            for pak in self.getPaksInPath(self.installPath):
+                os.remove(os.path.join(self.installPath, pak))
 
             # TODO do mod integration
 
-            # TODO list mods and commands
+            # load all previously active mods back into mod path (with changes)
+            for mod in self.mods:
+                if mod["installed"] or mod["always_active"]:
+                    shutil.copyfile(os.path.join(
+                        self.downloadPath, mod["filename"]), os.path.join(self.installPath, mod["filename"]))
+
+            # write modconfig.json
+            config = []
+            for mod in self.mods:
+                config.append({
+                    "mod_id": mod["metadata"]["mod_id"],
+                    "update": mod["update"],
+                    "always_active": mod["always_active"]
+                })
+            with open(os.path.join(self.downloadPath, "modconfig.json"), 'r+') as f:
+                f.truncate(0)
+            with open(os.path.join(self.downloadPath, "modconfig.json"), 'w') as f:
+                f.write(json.dumps({"mods": config}))
+
+            # list mods and commands
             tabelData = []
             tabelData.append(
-                ["active", "mod name", "version", "author", "mod id"])
+                ["active", "mod name", "version", "author", "mod id", "update", "always active"])
 
             for mod in self.mods:
-                tabelData.append(
-                    [mod["installed"], mod["metadata"]["name"], mod["metadata"]["version"], mod["metadata"]["author"], mod["metadata"]["mod_id"]])
+                tabelData.append([
+                    mod["installed"],
+                    mod["metadata"]["name"],
+                    mod["metadata"]["version"],
+                    mod["metadata"]["author"],
+                    mod["metadata"]["mod_id"],
+                    mod["update"],
+                    mod["always_active"]
+                ])
 
             table = SingleTable(tabelData, "Available mods")
+            print("")
             print(table.table)
+            print("commands: exit, (activate, deactivate, server)")
 
             # TODO start cli for moving mods and server config
             cmd = input("> ")
